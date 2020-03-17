c96b2d84cf776bd21c96f462d085e78795252ddd
Sylry <Sylry@users.noreply.github.com>
2020-03-17 16:08:39 +0100

    Use custom Validator to fix form fields mapping

--- Validator/Constraints/EmbeddedAddressFormatConstraint.php
+++ Validator/Constraints/EmbeddedAddressFormatConstraint.php
@@ -15,14 +15,6 @@ class EmbeddedAddressFormatConstraint extends AddressFormatConstraint
     /**
      * {@inheritdoc}
      */
-    public function validatedBy()
-    {
-        return AddressFormatConstraintValidator::class;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
     public function getTargets()
     {
         return self::PROPERTY_CONSTRAINT;
