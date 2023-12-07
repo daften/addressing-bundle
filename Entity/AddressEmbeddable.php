53d19e2a3d54c26e9e94ac7ecd7a30c64387fe1e
Rafal Kozlowski <rafal.kozlowski@smarteye.eu>
2023-12-07 15:28:01 +0100

    Add default values to Address fields

--- Entity/AddressEmbeddable.php
+++ Entity/AddressEmbeddable.php
@@ -13,67 +13,69 @@ class AddressEmbeddable extends Address
     /**
      * @ORM\Column(type="string", nullable=true)
      */
-    protected string $locale;
+    protected string $locale = 'und';
 
     /**
      * @ORM\Column(type="string", nullable=true)
      */
-    protected string $givenName;
+    protected string $givenName = '';
 
     /**
      * @ORM\Column(type="string", nullable=true)
      */
-    protected string $additionalName;
+    protected string $additionalName = '';
 
     /**
      * @ORM\Column(type="string", nullable=true)
      */
-    protected string $familyName;
+    protected string $familyName = '';
 
     /**
      * @ORM\Column(type="string", nullable=true)
      */
-    protected string $organization;
+    protected string $organization = '';
 
     /**
      * @ORM\Column(type="string", nullable=true)
      */
-    protected string $addressLine1;
+    protected string $addressLine1 = '';
 
     /**
      * @ORM\Column(type="string", nullable=true)
      */
-    protected string $addressLine2;
+    protected string $addressLine2 = '';
+
+    protected string $addressLine3 = '';
 
     /**
      * @ORM\Column(type="string", nullable=true)
      */
-    protected string $postalCode;
+    protected string $postalCode = '';
 
     /**
      * @ORM\Column(type="string", nullable=true)
      */
-    protected string $sortingCode;
+    protected string $sortingCode = '';
 
     /**
      * @ORM\Column(type="string", nullable=true)
      */
-    protected string $locality;
+    protected string $locality = '';
 
     /**
      * @ORM\Column(type="string", nullable=true)
      */
-    protected string $dependentLocality;
+    protected string $dependentLocality = '';
 
     /**
      * @ORM\Column(type="string", nullable=true)
      */
-    protected string $administrativeArea;
+    protected string $administrativeArea = '';
 
     /**
      * @ORM\Column(type="string", length=2, nullable=true)
      */
-    protected string $countryCode;
+    protected string $countryCode = '';
 
     public function __toString(): string
     {
@@ -93,210 +95,67 @@ class AddressEmbeddable extends Address
         ]));
     }
 
-    /**
-     * @return string
-     */
-    public function getLocale(): string
-    {
-        return $this->locale;
-    }
-
-    /**
-     * @param string $locale
-     */
-    public function setLocale(string $locale = null): void
+    public function setLocale(string $locale): void
     {
         $this->locale = $locale;
     }
 
-    /**
-     * @return string
-     */
-    public function getAdditionalName(): string
-    {
-        return $this->additionalName;
-    }
-
-    /**
-     * @param string $additionalName
-     */
-    public function setAdditionalName(string $additionalName = null): void
+    public function setAdditionalName(string $additionalName): void
     {
         $this->additionalName = $additionalName;
     }
 
-    /**
-     * @return string
-     */
-    public function getGivenName(): string
-    {
-        return $this->givenName;
-    }
-
-    /**
-     * @param string $givenName
-     */
-    public function setGivenName(string $givenName = null): void
+    public function setGivenName(string $givenName): void
     {
         $this->givenName = $givenName;
     }
 
-    /**
-     * @return string
-     */
-    public function getFamilyName(): string
-    {
-        return $this->familyName;
-    }
-
-    /**
-     * @param string $familyName
-     */
-    public function setFamilyName(string $familyName = null): void
+    public function setFamilyName(string $familyName): void
     {
         $this->familyName = $familyName;
     }
 
-    /**
-     * @return string
-     */
-    public function getOrganization(): string
-    {
-        return $this->organization;
-    }
-
-    /**
-     * @param string $organization
-     */
-    public function setOrganization(string $organization = null): void
+    public function setOrganization(string $organization): void
     {
         $this->organization = $organization;
     }
 
-    /**
-     * @return string
-     */
-    public function getAddressLine1(): string
-    {
-        return $this->addressLine1;
-    }
-
-    /**
-     * @param string $addressLine1
-     */
-    public function setAddressLine1($addressLine1 = null): void
+    public function setAddressLine1(string $addressLine1): void
     {
         $this->addressLine1 = $addressLine1;
     }
 
-    /**
-     * @return string
-     */
-    public function getAddressLine2(): string
-    {
-        return $this->addressLine2;
-    }
-
-    /**
-     * @param string $addressLine2
-     */
-    public function setAddressLine2(string $addressLine2 = null): void
+    public function setAddressLine2(string $addressLine2): void
     {
         $this->addressLine2 = $addressLine2;
     }
 
-    /**
-     * @return string
-     */
-    public function getPostalCode(): string
-    {
-        return $this->postalCode;
-    }
-
-    /**
-     * @param string $postalCode
-     */
-    public function setPostalCode(string $postalCode = null): void
+    public function setPostalCode(string $postalCode): void
     {
         $this->postalCode = $postalCode;
     }
 
-    /**
-     * @return string
-     */
-    public function getSortingCode(): string
-    {
-        return $this->sortingCode;
-    }
-
-    /**
-     * @param string $sortingCode
-     */
-    public function setSortingCode(string $sortingCode = null): void
+    public function setSortingCode(string $sortingCode): void
     {
         $this->sortingCode = $sortingCode;
     }
 
-    /**
-     * @return string
-     */
-    public function getLocality(): string
-    {
-        return $this->locality;
-    }
-
-    /**
-     * @param string $locality
-     */
-    public function setLocality(string $locality = null): void
+    public function setLocality(string $locality): void
     {
         $this->locality = $locality;
     }
 
-    /**
-     * @return string
-     */
-    public function getDependentLocality(): string
-    {
-        return $this->dependentLocality;
-    }
-
-    /**
-     * @param string $dependentLocality
-     */
-    public function setDependentLocality(string $dependentLocality = null): void
+    public function setDependentLocality(string $dependentLocality): void
     {
         $this->dependentLocality = $dependentLocality;
     }
 
-    /**
-     * @return string
-     */
-    public function getAdministrativeArea(): string
-    {
-        return $this->administrativeArea;
-    }
-
-    /**
-     * @param string $administrativeArea
-     */
-    public function setAdministrativeArea(string $administrativeArea = null): void
+    public function setAdministrativeArea(string $administrativeArea): void
     {
         $this->administrativeArea = $administrativeArea;
     }
 
-    /**
-     * @return string
-     */
-    public function getCountryCode(): string
-    {
-        return $this->countryCode;
-    }
-
-    /**
-     * @param string $countryCode
-     */
-    public function setCountryCode(string $countryCode = NULL): void
+    public function setCountryCode(string $countryCode): void
     {
         $this->countryCode = $countryCode;
     }
